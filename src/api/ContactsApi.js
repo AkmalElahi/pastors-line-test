66d16179c855a3d33f02f020056a91d4824e60d5
akmalelahi <akmalelahishaikh@gmail.com>
2020-12-15 22:00:19 +0500

    fix:move token in env file

--- src/api/ContactsApi.js
+++ src/api/ContactsApi.js
@@ -1,22 +1,5 @@
 import axios from 'axios';
 import { BASE_URL } from '../configs/configs';
-import BaseApi from './BaseApi';
-
-// class ContactsApi extends BaseApi {
-//   getContacts(params) {
-//     return axios.get(
-//       BASE_URL,
-//       {
-//         headers: {
-//           'Authorization': this.getToken()
-//         },
-//         params
-//       },
-//     );
-//   }
-// }
-
-// export default new ContactsApi();
 
 const getContactsService = async (opts) => {
   const { data } = await axios.get(BASE_URL,
